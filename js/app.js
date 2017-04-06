d41b6e7c8c69c5ff1474c6d3197d8dbb398f6a63
Andres Armendariz <andres.armsot@icloud.com>
2017-04-06 14:06:16 -0600

    modificacion en index.html

--- js/app.js
+++ js/app.js
@@ -34,42 +34,42 @@
         img: "./public/img/gem-03.gif"}, 
 
         {name: "Quartz",
-        price: 4.95,
+        price: 1.99,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque commodi, dolorum veniam ullam, explicabo deserunt, dolor soluta maxime est aliquam dignissimos, optio! Blanditiis, impedit, aliquid repudiandae vero doloremque voluptates pariatur.",
         canPurchase: false, //la forma de la que escriben es la forma en la que programa.  
         soldOut: false,
         img: "./public/img/gem-04.gif"},
 
         {name: "Agata",
-        price: 4.95,
+        price: 1.25,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque commodi, dolorum veniam ullam, explicabo deserunt, dolor soluta maxime est aliquam dignissimos, optio! Blanditiis, impedit, aliquid repudiandae vero doloremque voluptates pariatur.",
         canPurchase: false, //la forma de la que escriben es la forma en la que programa.  
         soldOut: false,
         img: "./public/img/gem-05.gif"},
 
         {name: "Pink Diamond",
-        price: 4.95,
+        price: 10.95,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque commodi, dolorum veniam ullam, explicabo deserunt, dolor soluta maxime est aliquam dignissimos, optio! Blanditiis, impedit, aliquid repudiandae vero doloremque voluptates pariatur.",
         canPurchase: false, //la forma de la que escriben es la forma en la que programa.  
         soldOut: true,
         img: "./public/img/gem-06.gif"},
 
         {name: "White Diamond",
-        price: 4.95,
+        price: 7.99,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque commodi, dolorum veniam ullam, explicabo deserunt, dolor soluta maxime est aliquam dignissimos, optio! Blanditiis, impedit, aliquid repudiandae vero doloremque voluptates pariatur.",
         canPurchase: false, //la forma de la que escriben es la forma en la que programa.  
         soldOut: false,
         img: "./public/img/gem-07.gif"},
 
         {name: "Blue Diamond",
-        price: 4.95,
+        price: 7.75,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque commodi, dolorum veniam ullam, explicabo deserunt, dolor soluta maxime est aliquam dignissimos, optio! Blanditiis, impedit, aliquid repudiandae vero doloremque voluptates pariatur.",
         canPurchase: false, //la forma de la que escriben es la forma en la que programa.  
         soldOut: false,
         img: "./public/img/gem-08.gif"},
 
         {name: "Ruby",
-        price: 4.95,
+        price: .99,
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque commodi, dolorum veniam ullam, explicabo deserunt, dolor soluta maxime est aliquam dignissimos, optio! Blanditiis, impedit, aliquid repudiandae vero doloremque voluptates pariatur.",
         canPurchase: false, //la forma de la que escriben es la forma en la que programa.  
         soldOut: false,
